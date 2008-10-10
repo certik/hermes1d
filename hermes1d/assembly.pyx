58da2a029ad8682fd1a2e83c2d7c7c5b76dba8ff
Ondrej Certik <ondrej@certik.cz>
2008-10-10 16:57:45 +0200

    for optimizations.

--- hermes1d/assembly.pyx
+++ hermes1d/assembly.pyx
@@ -2,9 +2,12 @@ from numpy import zeros
 from numpy cimport ndarray
 from numpy cimport int_t as DTYPE_t
 
+cimport cython
 
+@cython.boundscheck(False)
 def f(ndarray[DTYPE_t] x):
-    cdef ndarray y = zeros(len(x), dtype="double")
+    cdef ndarray[DTYPE_t] y = zeros(len(x), dtype="double")
+    cdef int i
     for i in range(len(x)):
         y[i] = x[i] + 2
     return y

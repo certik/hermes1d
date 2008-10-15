bf7192ce10d34324f6dd047c697dbee9af763e05
Ondrej Certik <ondrej@certik.cz>
2008-10-15 09:31:01 +0200

    Construct both A and B

--- hermes1d/cassembly.cpp
+++ hermes1d/cassembly.cpp
@@ -6,9 +6,14 @@ void A::print_info()
     printf("# nodes: %d\n", this->nmesh);
 }
 
-void set_dof(int i, int j, double value)
+void set_dof_A(int i, int j, double value)
 {
-    printf("%d %d %f\n", i, j, value);
+    printf("A: %d %d %f\n", i, j, value);
+}
+
+void set_dof_B(int i, int j, double value)
+{
+    printf("B: %d %d %f\n", i, j, value);
 }
 
 void A::assemble()
@@ -17,19 +22,26 @@ void A::assemble()
     double h0, h1, a0 = 1.0, a1 = 1.0;
     h0 = this->h(0);
     h1 = this->h(1);
-    set_dof(0, 0, a1*(1./h0+1./h1)+a0*(h0/3. + h1/3.));
-    set_dof(0, 1, -a1/h1 + a0*h1/6.);
+    set_dof_A(0, 0, a1*(1./h0+1./h1));
+    set_dof_A(0, 1, -a1/h1);
+    set_dof_B(0, 0, a0*(h0/3. + h1/3.));
+    set_dof_B(0, 1, a0*h1/6.);
     int i;
     for (i=1; i<this->nmesh-2; i++) {
         h0 = this->h(i);
         h1 = this->h(i+1);
-        set_dof(i, i-1, -a1/h0 + a0*h0/6);
-        set_dof(i, i, a1*(1./h0 + 1./h1) + a0*(h0/3.+h1/3.));
-        set_dof(i, i+1, -a1/h1 + a0*h1/6);
+        set_dof_A(i, i-1, -a1/h0);
+        set_dof_A(i, i, a1*(1./h0 + 1./h1));
+        set_dof_A(i, i+1, -a1/h1);
+        set_dof_B(i, i-1, a0*h0/6);
+        set_dof_B(i, i, a0*(h0/3.+h1/3.));
+        set_dof_B(i, i+1, a0*h1/6);
     }
     i = this->nmesh-2;
     h0 = this->h(i-1);
     h1 = this->h(i);
-    set_dof(i, i-1, -a1/h0 + a0*h0/6);
-    set_dof(i, i, a1*(1./h0 + 1./h1) + a0*(h0/3.+h1/3.));
+    set_dof_A(i, i-1, -a1/h0);
+    set_dof_A(i, i, a1*(1./h0 + 1./h1));
+    set_dof_B(i, i-1, a0*h0/6);
+    set_dof_B(i, i, a0*(h0/3.+h1/3.));
 }

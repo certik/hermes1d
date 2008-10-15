0534559be7f069a54e6ee8f4172205aad3f263b5
Ondrej Certik <ondrej@certik.cz>
2008-10-15 16:36:12 +0200

    refactoring

--- hermes1d/cassembly.cpp
+++ hermes1d/cassembly.cpp
@@ -54,17 +54,12 @@ double System::int_u_v(int i, int j)
 void System::assemble()
 {
     printf("assembling...\n");
-    double h0, h1, a0 = 1.0, a1 = 1.0;
-    h0 = this->h(0);
-    h1 = this->h(1);
-    set_dof_A(0, 0, a1*(1./h0+1./h1));
-    set_dof_A(0, 1, -a1/h1);
-    set_dof_B(0, 0, a0*(h0/3. + h1/3.));
-    set_dof_B(0, 1, a0*h1/6.);
+    set_dof_A(0, 0, int_grad_u_grad_v(0, 0));
+    set_dof_A(0, 1, int_grad_u_grad_v(0, 1));
+    set_dof_B(0, 0, int_u_v(0, 0));
+    set_dof_B(0, 1, int_u_v(0, 1));
     int i;
     for (i=1; i<this->nmesh-2; i++) {
-        h0 = this->h(i);
-        h1 = this->h(i+1);
         set_dof_A(i, i-1, int_grad_u_grad_v(i, i-1));
         set_dof_A(i, i, int_grad_u_grad_v(i, i));
         set_dof_A(i, i+1, int_grad_u_grad_v(i, i+1));
@@ -73,10 +68,8 @@ void System::assemble()
         set_dof_B(i, i+1, int_u_v(i, i+1));
     }
     i = this->nmesh-2;
-    h0 = this->h(i-1);
-    h1 = this->h(i);
-    set_dof_A(i, i-1, -a1/h0);
-    set_dof_A(i, i, a1*(1./h0 + 1./h1));
-    set_dof_B(i, i-1, a0*h0/6);
-    set_dof_B(i, i, a0*(h0/3.+h1/3.));
+    set_dof_A(i, i-1, int_grad_u_grad_v(i-1, i-1-1));
+    set_dof_A(i, i, int_grad_u_grad_v(i-1, i-1));
+    set_dof_B(i, i-1, int_u_v(i-1, i-1-1));
+    set_dof_B(i, i, int_u_v(i-1, i-1));
 }

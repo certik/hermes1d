465dd1c1401da846b69e0892d37e7f72361a77e5
Ondrej Certik <ondrej@certik.cz>
2008-10-15 16:30:05 +0200

    Refactoring.

--- hermes1d/cassembly.cpp
+++ hermes1d/cassembly.cpp
@@ -19,31 +19,35 @@ void System::set_dof_B(int i, int j, double value)
 
 double System::int_grad_u_grad_v(int i, int j)
 {
-    double a0 = 1.0, a1 = 1.0;
-    double hi = this->h(i);
-    double hi_plus_1 = this->h(i+1);
-    if (j == i - 1)
+    double a1 = 1.0;
+    if (j == i - 1) {
+        double hi = this->h(i);
         return -a1/hi;
-    else if (j == i)
+    } else if (j == i) {
+        double hi = this->h(i);
+        double hi_plus_1 = this->h(i+1);
         return a1*(1./hi + 1./hi_plus_1);
-    else if (j == i + 1)
+    } else if (j == i + 1) {
+        double hi_plus_1 = this->h(i+1);
         return -a1/hi_plus_1;
-    else
+    } else
         return 0.;
 }
 
 double System::int_u_v(int i, int j)
 {
-    double a0 = 1.0, a1 = 1.0;
-    double hi = this->h(i);
-    double hi_plus_1 = this->h(i+1);
-    if (j == i - 1)
+    double a0 = 1.0;
+    if (j == i - 1) {
+        double hi = this->h(i);
         return a0*hi/6;
-    else if (j == i)
+    } else if (j == i) {
+        double hi = this->h(i);
+        double hi_plus_1 = this->h(i+1);
         return a0*(hi/3.+hi_plus_1/3.);
-    else if (j == i + 1)
+    } else if (j == i + 1) {
+        double hi_plus_1 = this->h(i+1);
         return a0*hi_plus_1/6;
-    else
+    } else
         return 0.;
 }
 

8f3d67f229d80743a873093e165deab029d9c67b
Ondrej Certik <ondrej@certik.cz>
2009-03-07 08:13:49 -0500

    assign_dofs() implemented

--- hermes1d/hermes1d.py
+++ hermes1d/hermes1d.py
@@ -18,6 +18,7 @@ class Element(object):
     def __init__(self, x1, x2, order=1):
         self._nodes = (x1, x2)
         self._order = order
+        self._dofs = [-1]*(order+1)
 
     @property
     def nodes(self):
@@ -26,6 +27,30 @@ class Element(object):
     @property
     def order(self):
         return self._order
+
+    @property
+    def dofs(self):
+        return self._dofs
+
+    def assign_dofs(self, local_dofs, global_dofs):
+        """
+        Sets the global degrees of freedom corresponding to the local shape
+        functions.
+
+        Example 1:
+        >>> e = Element(n1, n2, order=2)
+        >>> e.set_dofs([0, 1, 2], [4, 331, 18])
+
+        Example 1:
+        >>> e = Element(n1, n2, order=2)
+        >>> e.set_dofs([0, 1], [4, 331])
+        >>> e.set_dofs([2], [18])
+        """
+        for l, g in zip(local_dofs, global_dofs):
+            if l >= len(self._dofs):
+                print l
+                raise ValueError("local dof too high")
+            self._dofs[l] = g
 
 class Mesh(object):
     """
@@ -44,3 +69,7 @@ class Mesh(object):
     @property
     def elements(self):
         return self._elements
+
+    def assign_dofs(self):
+        for i in range(len(self._nodes)-1):
+            self._elements[i].assign_dofs([0, 1], [i, i+1])
--- hermes1d/tests/test_mesh.py
+++ hermes1d/tests/test_mesh.py
@@ -19,20 +19,41 @@ def test_element2():
     assert e.nodes == (n1, n2)
 
 def test_mesh1():
+    n0 = Node(1)
+    n1 = Node(3)
+    n2 = Node(4)
+    n3 = Node(5)
+    e0 = Element(n0, n1)
+    e1 = Element(n1, n2)
+    e2 = Element(n2, n3)
+    nodes = (n0, n1, n2, n3)
+    elements = (e0, e1, e2)
+    m = Mesh(nodes, elements)
+    assert m.nodes == nodes
+    assert m.elements == elements
+    assert m.nodes[0] == n0
+    assert m.nodes[3] == n3
+
+def test_mesh2():
     n1 = Node(1)
     n2 = Node(3)
     n3 = Node(4)
     n4 = Node(5)
-    e1 = Element(n1, n2)
-    e2 = Element(n2, n3)
-    e3 = Element(n3, n4)
+    e1 = Element(n1, n2, order=1)
+    e2 = Element(n2, n3, order=1)
+    e3 = Element(n3, n4, order=1)
     nodes = (n1, n2, n3, n4)
     elements = (e1, e2, e3)
     m = Mesh(nodes, elements)
-    assert m.nodes == nodes
-    assert m.elements == elements
+    m.assign_dofs()
+    assert m.elements[0].dofs[0] == 0
+    assert m.elements[0].dofs[1] == 1
+    assert m.elements[1].dofs[0] == 1
+    assert m.elements[1].dofs[1] == 2
+    assert m.elements[2].dofs[0] == 2
+    assert m.elements[2].dofs[1] == 3
 
-def test_mesh2():
+def _test_mesh3():
     n1 = Node(1)
     n2 = Node(3)
     n3 = Node(4)
@@ -40,9 +61,16 @@ def test_mesh2():
     e1 = Element(n1, n2, order=2)
     e2 = Element(n2, n3, order=2)
     e3 = Element(n3, n4, order=2)
-    assert e3.order == 2
     nodes = (n1, n2, n3, n4)
     elements = (e1, e2, e3)
     m = Mesh(nodes, elements)
-    assert m.nodes == nodes
-    assert m.elements == elements
+    m.assign_dofs()
+    assert m.elements[0].dofs[0] == 0
+    assert m.elements[0].dofs[1] == 1
+    assert m.elements[0].dofs[2] == 6
+    assert m.elements[1].dofs[0] == 2
+    assert m.elements[1].dofs[1] == 3
+    assert m.elements[1].dofs[2] == 7
+    assert m.elements[2].dofs[0] == 4
+    assert m.elements[2].dofs[1] == 5
+    assert m.elements[2].dofs[2] == 8
